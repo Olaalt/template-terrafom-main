c17a28180bf7137fec3ac8abc94fd36fb017897c
Oladele Williams <onelovewilliams0310@gmail.com>
2024-03-12 22:32:05 -0400

    change sh file

--- script.sh
+++ script.sh
@@ -1,8 +1,3 @@
-# update the ubuntu server
-sudo apt-get update
-sudo apt-get upgrade -y
-
-
 # install unzip
 
 sudo apt-get install unzip
@@ -63,20 +58,18 @@ sudo apt-get install helm
 
 # install jenkins
 
-sudo apt-get update
-
-sudo apt install openjdk-11-jre -y
+sudo apt install fontconfig openjdk-17-jre -y
 
 java -version
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt-get install jenkins -y
 
